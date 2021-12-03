e5b68de6fc37b06fb8a0cf74b0ec89bb5c47b281
Gustavo <gustavolcampos11@gmail.com>
2021-12-03 13:44:50 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,37 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro Livro1 = new Livro("O problema", 200);
+        System.out.println(Livro1);
+
         /*int a = 5;
         int b =3;
 
         System.out.println("Hello World!" + (a+b));*/
+    }
+}
+class Livro {
+    private String name;
+    private Integer nuuPag;
+
+    public Livro(String name, Integer nuuPag) {
+        this.name = name;
+        this.nuuPag = nuuPag;
+    }
+
+    public Integer getNuuPag() {
+        return nuuPag;
+    }
+
+    public void setNuuPag(Integer nuuPag) {
+        this.nuuPag = nuuPag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
